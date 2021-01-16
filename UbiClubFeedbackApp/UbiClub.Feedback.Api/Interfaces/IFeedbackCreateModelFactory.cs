ceb552f389f7dd608a62b1f6c725eb43584eccfc
ahmed amin <ahmed.amin@integrant.com>
2021-01-16 08:53:21 +0200

    Added FeedbackCreateModel Factory

---
+++ UbiClubFeedbackApp/UbiClub.Feedback.Api/Interfaces/IFeedbackCreateModelFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using UbiClub.Feedback.Core.Models;
+
+namespace UbiClub.Feedback.Api.Interfaces
+{
+    public interface IFeedbackCreateModelFactory
+    {
+        Task<FeedbackCreateModel> CreateAsync(HttpRequest req, Guid? sessionId);
+    }
+}
---
+++ UbiClubFeedbackApp/UbiClub.Feedback.Api/ModelFactory/FeedbackCreateModelFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Newtonsoft.Json;
+using UbiClub.Feedback.Api.Interfaces;
+using UbiClub.Feedback.Core.Models;
+
+namespace UbiClub.Feedback.Api.ModelFactory
+{
+    internal class FeedbackCreateModelFactory : IFeedbackCreateModelFactory
+    {
+        private const string UserIdHeaderName = "Ubi-UserId";
+        public async Task<FeedbackCreateModel> CreateAsync(HttpRequest req, Guid? sessionId)
+        {
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var ratingModel = JsonConvert.DeserializeObject<RatingModel>(requestBody);
+            var model = new FeedbackCreateModel()
+            {
+                SessionId = sessionId,
+                Rating = ratingModel.Rating
+            };
+
+            if (req.Headers.TryGetValue(UserIdHeaderName, out var headerValue))
+            {
+                var strUserId = headerValue.FirstOrDefault();
+                if (!String.IsNullOrEmpty(strUserId)
+                    && Guid.TryParse(strUserId, out var userId))
+                {
+                    model.UserId = userId;
+                }
+            }
+
+            return model;
+        }
+    }
+}
