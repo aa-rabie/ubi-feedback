c4ed8e603a55f5a86c3db2f6b77269a7b0f56534
ahmed amin <ahmed.amin@integrant.com>
2021-01-16 08:52:43 +0200

    Added AutoMapper Profiles

---
+++ UbiClubFeedbackApp/UbiClub.Feedback.Api/Infrastructure/AutoMapper/FeedbackProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using UbiClub.Feedback.Core.Dto;
+using UbiClub.Feedback.Entities;
+
+namespace UbiClub.Feedback.Api.Infrastructure.AutoMapper
+{
+    class FeedbackProfile : Profile
+    {
+        public FeedbackProfile()
+        {
+            CreateMap<SessionFeedback, SessionFeedbackDto>().ReverseMap();
+        }
+    }
+}
