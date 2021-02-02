173f1edae0ab19eb66d51cd62a7067f2be31cba5
ahmed amin <ahmed.amin@integrant.com>
2021-02-02 07:59:58 +0200

    Adding IQueryCollection Fake Object

---
+++ UbiClubFeedbackApp/UbiClub.Feedback.Tests/Fakes/FakeQueryCollection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UbiClub.Feedback.Tests.Fakes
+{
+    public class FakeQueryCollection : Dictionary<string, StringValues> , IQueryCollection
+    {
+        public new StringValues this[string key] => base[key];
+
+        public new int Count => base.Count;
+
+        public new ICollection<string> Keys => base.Keys;
+
+        public new bool ContainsKey(string key)
+        {
+            return base.ContainsKey(key);
+        }
+
+        public new IEnumerator<KeyValuePair<string, StringValues>> GetEnumerator()
+        {
+            return base.GetEnumerator();
+        }
+
+        public new bool TryGetValue(string key, out StringValues value)
+        {
+            return base.TryGetValue(key, out value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return base.GetEnumerator() as IEnumerator;
+        }
+    }
+}
